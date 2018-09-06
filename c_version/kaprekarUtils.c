df2bbd4d15b84461331d34b306d6c7fe73a0aeba
mwagner99 <42562660+mwagner99@users.noreply.github.com>
2018-09-06 15:18:31 -0500

    Update kaprekarUtils.c

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -35,9 +35,9 @@ int isKaprekar(int n) {
   long first, second;
 
   //for each possible "split" of the square...
-  for(i=1; i<=numberOfDigits; i++) {
+  for(i=1; i<=numDigits; i++) { //numberOfDigits was not declared. numDigits is.
     //increase the modulus by a factor of 10
-    modulous *= 10;
+    modulus *= 10;  //modulus was spelled incorrectly.
 
     //split the square into two parts
     first = square / modulus;
